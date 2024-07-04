53b82b4853e4a9bf2c79e73d50c1340a86d4f47d
theendercore <theendercore@gmail.com>
2024-07-04 20:08:41 +0300

    - clean pt 2

--- src/main/java/org/teamvoided/nullium/mixin/GlowingFoodComponentsMixin.java
+++ src/main/java/org/teamvoided/nullium/mixin/GlowingFoodComponentsMixin.java
@@ -4,7 +4,6 @@ import net.minecraft.entity.effect.StatusEffectInstance;
 import net.minecraft.entity.effect.StatusEffects;
 import net.minecraft.item.FoodComponent;
 import net.minecraft.item.FoodComponents;
-import org.spongepowered.asm.mixin.Debug;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 

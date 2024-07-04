78a6c2aa550b9c5b8c4d4d82ef228bb2801cb3b6
theendercore <theendercore@gmail.com>
2024-07-04 19:22:17 +0300

    - renamed file - removed debug annotation

--- src/main/java/org/teamvoided/nullium/mixin/CopperBulbBlockMixin.java
+++ src/main/java/org/teamvoided/nullium/mixin/CopperBulbBlockMixin.java
@@ -7,20 +7,18 @@ import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.random.RandomGenerator;
 import net.minecraft.world.World;
-import org.spongepowered.asm.mixin.Debug;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Debug(export = true)
 @Mixin(WaxedCopperBulbBlock.class)
-public abstract class WaxedCopperBulbBlockMixin extends Block {
+public abstract class CopperBulbBlockMixin extends Block {
     @Shadow
     public abstract void setState(BlockState state, ServerWorld world, BlockPos pos);
 
-    public WaxedCopperBulbBlockMixin(Settings settings) {
+    public CopperBulbBlockMixin(Settings settings) {
         super(settings);
     }
 

d660ec5b2cad4b38e078d1e24f5fa787cbe692a4
theendercore <theendercore@gmail.com>
2024-07-04 17:01:58 +0300

    - mixins was not added to the mixin file

--- src/main/java/org/teamvoided/nullium/mixin/EndermanEntityMixin.java
+++ src/main/java/org/teamvoided/nullium/mixin/EndermanEntityMixin.java
@@ -10,31 +10,33 @@ import net.minecraft.world.LocalDifficulty;
 import net.minecraft.world.ServerWorldAccess;
 import net.minecraft.world.World;
 import org.jetbrains.annotations.Nullable;
+import org.spongepowered.asm.mixin.Debug;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.teamvoided.nullium.module.HolderMan;
 
-@SuppressWarnings({"unused", "UnusedMixin"})
+@Debug(export = true)
+@SuppressWarnings("unused")
 @Mixin(EndermanEntity.class)
 abstract class EndermanEntityMixin extends MobEntity {
 
-	@Shadow
-	@Nullable
-	public abstract BlockState getCarriedBlock();
+    @Shadow
+    @Nullable
+    public abstract BlockState getCarriedBlock();
 
-	protected EndermanEntityMixin(EntityType<? extends MobEntity> entityType, World world) {
-		super(entityType, world);
-	}
+    protected EndermanEntityMixin(EntityType<? extends MobEntity> entityType, World world) {
+        super(entityType, world);
+    }
 
-	@Override
-	public EntityData initialize(ServerWorldAccess world, LocalDifficulty difficulty, SpawnReason spawnReason, @Nullable EntityData entityData) {
-		entityData = super.initialize(world, difficulty, spawnReason, entityData);
-		if (this.getCarriedBlock() == null && !SpawnReason.isSpawner(spawnReason)) {
-			HolderMan.getBlocks(world.toServerWorld(), (EndermanEntity) (Object) this);
-		}
-		return entityData;
+    @Override
+    public EntityData initialize(ServerWorldAccess world, LocalDifficulty difficulty, SpawnReason spawnReason, @Nullable EntityData entityData) {
+        entityData = super.initialize(world, difficulty, spawnReason, entityData);
+        if (this.getCarriedBlock() == null && !SpawnReason.isSpawner(spawnReason)) {
+            HolderMan.getBlocks(world.toServerWorld(), (EndermanEntity) (Object) this);
+        }
+        return entityData;
 
-	}
+    }
 
 
 }

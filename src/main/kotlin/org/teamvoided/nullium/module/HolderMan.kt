c67f0fd9b0a3a951962ee080ba7d2455abf3ce77
theendercore <theendercore@gmail.com>
2024-07-04 16:36:17 +0300

    - Holderman now references the right loot table

--- src/main/kotlin/org/teamvoided/nullium/module/HolderMan.kt
+++ src/main/kotlin/org/teamvoided/nullium/module/HolderMan.kt
@@ -5,13 +5,14 @@ import net.minecraft.entity.mob.EndermanEntity
 import net.minecraft.item.BlockItem
 import net.minecraft.loot.context.LootContextTypes
 import net.minecraft.server.world.ServerWorld
+import org.teamvoided.nullium.data.loot.NulliumLootTables
 import org.teamvoided.nullium.util.LootContextParameterSet
 import org.teamvoided.nullium.util.getLootTable
 
 object HolderMan {
     @JvmStatic
     fun getBlocks(world: ServerWorld, end: EndermanEntity) {
-        val states = world.getLootTable(Blocks.GILDED_BLACKSTONE.lootTableId)
+        val states = world.getLootTable(NulliumLootTables.ENDERMAN_HOLDS)
             .generateLoot(LootContextParameterSet(world, LootContextTypes.EMPTY))
             .mapNotNull {
                 if (it.item is BlockItem) (it.item as BlockItem).block.defaultState

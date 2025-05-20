3587e0734c9a86a50957f352ef4ccf364b44f7b8
TheEnderCore <theendercore@gmail.com>
2025-05-20 16:32:20 +0300

    - made feather falling work for all entities

--- src/main/java/org/teamvoided/nullium/mixin/FarmlandBlockMixin.java
+++ src/main/java/org/teamvoided/nullium/mixin/FarmlandBlockMixin.java
@@ -5,9 +5,9 @@ import net.minecraft.block.FarmlandBlock;
 import net.minecraft.enchantment.EnchantmentHelper;
 import net.minecraft.enchantment.Enchantments;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.EquipmentSlot;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.passive.VillagerEntity;
-import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.registry.RegistryKeys;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.village.VillagerProfession;
@@ -24,17 +24,16 @@ public class FarmlandBlockMixin {
 
     @Inject(method = "onLandedUpon", at = @At("HEAD"), cancellable = true)
     private void onLandedUpon(World world, BlockState state, BlockPos pos, Entity entity, float fallDistance, CallbackInfo ci) {
-        if (!world.isClient) {
-            var farmerRule = !getBoolRule(world, FRAMERS_TRAMPL);
-            var featherFallingRule = !getBoolRule(world, FEATHER_FALLING_TRAMPLE);
-            if ((farmerRule || featherFallingRule) && entity instanceof LivingEntity livingEntity) {
-                if (farmerRule && livingEntity instanceof VillagerEntity villager && villager.getVillagerData().getProfession() == VillagerProfession.FARMER) {
+        if (!world.isClient && entity instanceof LivingEntity livingEntity) {
+            if (!getBoolRule(world, FRAMERS_TRAMPL) && livingEntity instanceof VillagerEntity villager && villager.getVillagerData().getProfession() == VillagerProfession.FARMER) {
+                ci.cancel();
+            }
+            if (!getBoolRule(world, FEATHER_FALLING_TRAMPLE)) {
+                var item = livingEntity.getEquippedStack(EquipmentSlot.FEET);
+                var ff = world.getRegistryManager().get(RegistryKeys.ENCHANTMENT).getHolder(Enchantments.FEATHER_FALLING);
+                if (ff.isEmpty()) return;
+                if (!item.isEmpty() && EnchantmentHelper.getLevel(ff.get(), item) > 0) {
                     ci.cancel();
-                }
-                if (featherFallingRule && livingEntity instanceof PlayerEntity player) {
-                    var item = player.getInventory().armor.getFirst();
-                    var ff = world.getRegistryManager().get(RegistryKeys.ENCHANTMENT).getHolderOrThrow(Enchantments.FEATHER_FALLING);
-                    if (!item.isEmpty() && EnchantmentHelper.getLevel(ff, item) > 0) ci.cancel();
                 }
             }
         }
